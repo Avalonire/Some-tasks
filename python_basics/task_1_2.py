bf8c1a1ddebadf22f5c9543a245bd32610bb4dcc
Alex <gofavalon@gmail.com>
2021-12-26 14:51:28 +0300

    try to task 2 :(

--- python_basics/task_1_2.py
+++ python_basics/task_1_2.py
@@ -4,3 +4,23 @@
 
 # date = input('введите дату в формате dd.mm.yyyy: ')
 date = '02.11.2013'
+date = date.split('.')
+
+# print(date)
+# result = []
+# item = result[0]
+# if int(item) == 1:
+#     result.append('первое')
+# elif int(item) == 2:
+#     result.append('второе')
+# elif int(item) == 3:
+#     result.append('третье')
+# elif int(item) == 4:
+#     result.append('четвертое')
+#     if int(month) == 1:
+#         result.append('января')
+#     elif int(month) == 2:
+#         result.append('февраля')
+# result.append(date[2])
+#
+# print(str(result), 'года')
