6e987db0ffc19faafe5b7e1a4df3256c17789bad
Alex <gofavalon@gmail.com>
2022-01-07 01:30:52 +0300

    task 1.2

--- python_basics/some task 1.1.py
+++ python_basics/some task 1.1.py
@@ -5,3 +5,11 @@ def ctz(name, age, city):
 ctz('Alex', 30, 'Москва')
 print('-' * 100)
 
+numbers = [1, 2, 44, 545, 2, 45]
+numbers_2 = [23, 12, 33, 1, 56, 77]
+
+def num(list):
+    return max(list)
+
+
+print(num(numbers))
