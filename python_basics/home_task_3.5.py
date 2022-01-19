8ad68e557b17041738fc9d8b4269d8a956e73f01
Alex <gofavalon@gmail.com>
2022-01-19 11:02:50 +0300

    some upd hw 3.5

--- python_basics/home_task_3.5.py
+++ python_basics/home_task_3.5.py
@@ -1,3 +1,15 @@
+# 5. Реализовать функцию get_jokes(), возвращающую n шуток, сформированных из трех случайных слов, взятых из трёх списков (по одному из каждого):
+#
+#         Например:
+#
+# >>> get_jokes(2)
+# ["лес завтра зеленый", "город вчера веселый"]
+#
+# Документировать код функции.
+# Сможете ли вы добавить еще один аргумент — флаг, разрешающий или запрещающий повторы слов в шутках (когда каждое слово можно использовать только в одной шутке)?
+# Сможете ли вы сделать аргументы именованными?
+
+# вопрос: где оставлять списки? внутри функции или снаружи?
 import random
 
 
@@ -9,16 +21,15 @@ def get_jokes(joke_count=1.0, rep_words=None):
     rep_words -- optional, unique words (default None)
 
     Limits:
-    unique jokes are limited by the length of local sequences;
-    local sequences must be equal length;
+    unique jokes are limited by the length of local sequences
+
     """
     nouns = ["автомобиль", "лес", "огонь", "город", "дом"]
     adverbs = ["сегодня", "вчера", "завтра", "позавчера", "ночью"]
     adjectives = ["веселый", "яркий", "зеленый", "утопичный", "мягкий"]
     result = []
     if rep_words == 'yes' or rep_words == 'y':
-        limit_len = len(nouns)
-        for i in range(len(nouns)):
+        for i in range():
             noun = random.choice(nouns)
             adv = random.choice(adverbs)
             adj = random.choice(adjectives)
@@ -26,7 +37,7 @@ def get_jokes(joke_count=1.0, rep_words=None):
             adverbs.remove(adv)
             adjectives.remove(adj)
             result.append(f'{noun} {adv} {adj}')
-        print(f'You have only {limit_len} unique joke!')
+        print('You have only 5 unique joke!')
     else:
         for joke in range(int(joke_count)):
             result.append(f'{random.choice(nouns)} {random.choice(adverbs)} {random.choice(adjectives)}')
