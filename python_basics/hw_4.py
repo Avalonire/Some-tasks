72f2990f2dba633998763f94e7375ea1d04777dd
Alex <gofavalon@gmail.com>
2022-01-21 18:35:01 +0300

    some 4 hw upd

--- python_basics/hw_4.py
+++ python_basics/hw_4.py
@@ -7,5 +7,5 @@ else:
     print(f"Request 123: {currency_rates('123')}")
     print(f"Request uSD: {currency_rates('uSD')}")
     print(f"Request EUR: {currency_rates('EUR')}")
-    print(f"Request 000: {currency_rates(000)}")
+    print(f"Request int 000: {currency_rates(000)}")
     print(f"Request azn: {currency_rates('azn')}")
