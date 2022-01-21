de186465892c76f781d5e522b8175e8f84dc3b4c
Alex <gofavalon@gmail.com>
2022-01-21 18:30:25 +0300

    some 4 hw upd

--- python_basics/hw_4.py
+++ python_basics/hw_4.py
@@ -1,12 +1,11 @@
+import sys
 from hw_4_2345 import currency_rates
 
-
-def main(argv):
-    program, args = argv
-    print(currency_rates(args))
-
-
-if __name__ == '__main__':
-    import sys
-
-    exit(main(sys.argv))
+if len(sys.argv) > 1:
+    print(currency_rates(sys.argv[1]))
+else:
+    print(f"Request 123: {currency_rates('123')}")
+    print(f"Request uSD: {currency_rates('uSD')}")
+    print(f"Request EUR: {currency_rates('EUR')}")
+    print(f"Request 000: {currency_rates(000)}")
+    print(f"Request azn: {currency_rates('azn')}")
