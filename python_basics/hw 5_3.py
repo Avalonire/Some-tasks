e693e39d27c344b62dff61b3907a925e1d3e4320
Alex <gofavalon@gmail.com>
2022-01-23 18:10:13 +0300

    5.3?

---
+++ python_basics/hw 5_3.py
@@ -0,0 +1,21 @@
+tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Андрей', 'Карл', 'Вадим']
+klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
+
+
+def seq_len_equal(long, short):
+    short.extend([None for i in range(len(long) - len(short))])
+    return range(len(long))
+
+
+tutors_gen = ((tutors[i], klasses[i]) for i in seq_len_equal(tutors, klasses))
+
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
+print(next(tutors_gen))
