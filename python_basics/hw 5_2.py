707705768a3eb96aef86b2e9b2c05667866dd972
Alex <gofavalon@gmail.com>
2022-01-25 00:18:29 +0300

    hw 5 upd

--- python_basics/hw 5_2.py
+++ python_basics/hw 5_2.py
@@ -2,4 +2,8 @@ def odd_nums(arg):
     return (num for num in range(1, arg + 1, 2))
 
 
-print(next(odd_nums(15)))
+odd_nums_15 = odd_nums(15)
+print(next(odd_nums_15))
+print(next(odd_nums_15))
+print(next(odd_nums_15))
+print(next(odd_nums_15))
--- python_basics/hw 5_3.py
+++ python_basics/hw 5_3.py
@@ -1,14 +1,8 @@
 tutors = ['Иван', 'Анастасия', 'Петр', 'Сергей', 'Дмитрий', 'Борис', 'Елена', 'Андрей', 'Карл', 'Вадим']
 klasses = ['9А', '7В', '9Б', '9В', '8Б', '10А', '10Б', '9А']
 
-
-def seq_len_equal(long, short):
-    """ Makes the length of the two sequences equal with add None to short """
-    short.extend([None for i in range(len(long) - len(short))])
-    return range(len(long))
-
-
-tutors_gen = ((tutors[i], klasses[i]) for i in seq_len_equal(tutors, klasses))
+klasses.extend([None for i in range(len(tutors) - len(klasses))])
+tutors_gen = ((tutors[i], klasses[i]) for i in range(len(tutors)))
 
 print(type(tutors_gen))
 for ind in range(len(tutors)):
